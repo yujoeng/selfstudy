795482cc97b64f74eca5ea4745d4d3ea373704ce
yujoeng <dnwjd812@gmail.com>
2025-08-20 18:01:04 +0900

    0820

--- BAEKJOON/BAEKJOON.py
+++ BAEKJOON/BAEKJOON.py
@@ -1,19 +1,6 @@
+year = int(input())
 
-# n1 = int(input())
-# n2 = input()
-
-# print(n1 * (n2 %10))
-# print(n1 * (n2 // 10)%10)
-# print(n1 * (n2 // 100))
-
-# print(n1 * n2)
-
-
-#for 반복문을 사용하여 b의 자릿수를 뒤에서부터 하나씩 가져와서 a와 곱합니다. 
-a = int(input())
-b = input()
-
-for i in range(3, 0, -1):
-    print(a * int(b[i-1]))
-
-print(a * int(b))
+if ((year %4 == 0 and year 100% == 0) or year 400% == 0 ): 
+    print('1')
+else: 
+    print('0')
