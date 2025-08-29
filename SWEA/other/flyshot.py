dbe7c976516ebb970b8d92b309836060292a09ec
yujoeng <dnwjd812@gmail.com>
2025-08-29 20:19:46 +0900

    노트북까지 최종 정리 완

---
+++ SWEA/other/flyshot.py
@@ -0,0 +1,19 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N,M = map(int,input().split())
+    arr = [list(map(int,input().split()))for _ in range(N)]
+
+    max_v = 0
+
+
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            S = 0
+            for p in range(M):
+                for q in range(M):
+
+
+
+
+
+    print(f'#{tc} {max_v}')
