70841af23483b7ebbd2559dc81cd49438576973d
yujoeng <dnwjd812@gmail.com>
2025-10-20 09:42:34 +0900

    file clear

--- memory_note.py
+++ memory_note.py
@@ -1,17 +1,21 @@
-def f(idx, total):
-    global min_answer
-    if total >= b:
-        min_answer = min(min_answer, total)
-        return
-    if idx == n:
-        return
-    f(idx + 1, total + height[idx])
-    f(idx + 1, total)
+'''
+문제
+두 양의 정수가 주어졌을 때, 첫 번째 수가 두 번째 수보다 큰지 구하는 프로그램을 작성하시오.
 
-t = int(input())
-for tc in range(1, T+1):
-    n, b = map(int, input().split())
-    height = list(map(int, input().split()))
-    min_answer = 21e8
-    f(0,0)
-    print(f'#{tc} {min_answer - b}')
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 
+두 정수가 주어진다. 두 수는 백만보다 작거나 같은 양의 정수이다. 입력의 마지막 줄에는 0이 두 개 주어진다.
+
+출력
+각 테스트 케이스마다, 첫 번째 수가 두 번째 수보다 크면 Yes를, 아니면 No를 한 줄에 하나씩 출력한다.
+'''
+
+a, b = map(int,input().split())
+
+while True:
+    a = 0
+    if a > b:
+        print('yes')
+    
+    elif a < b:
+        print('no')
